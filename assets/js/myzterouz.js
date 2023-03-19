15df7d473ada1931e528636dcce84e3a4a170e43
Ronnie <102843994+Myzterouz@users.noreply.github.com>
2023-03-19 13:58:09 +0800

    Add files via upload

--- assets/js/myzterouz.js
+++ assets/js/myzterouz.js
@@ -1,10 +1,17 @@
 $( document ).ready(function() {
     setup();
     console.log( "ready!" );
+    $("#backButton").on("click", function(){
+        goBack();
+    });    
+    $("img.card-img-top").on("click", function() {
+        loadhtml("info");
+    });      
 });
 
 
 function setup(){
+    $("#backButton").hide();
     $("#img1").attr("src",img1);
     $("#img2").attr("src",img2);
     $('#img1').parent().find('.card-title').text(user1);
@@ -13,4 +20,23 @@ function setup(){
     $('#img2').parent().find('.card-title').addClass("text-uppercase");
     $('#img1').parent().find('.card-text').text(text1);
     $('#img2').parent().find('.card-text').text(text2);
+    $("#backButton").attr("href",indexhtml);
+}
+
+function  loadhtml(mode){
+    const container = $(".container");
+    fetch(`./views/${mode}.html`)
+        .then(res => {
+            if(res.ok){
+                return res.text();
+            }
+        })
+        .then(html =>{
+            $("#backButton").show();       
+            container.html(html);
+        });
+}
+
+function goBack() {
+    $("#backButton").hide();
 }
