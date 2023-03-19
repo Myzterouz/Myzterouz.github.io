94fd0ee026fcccaa99226ce3c0fd17a92277e980
Ronnie <102843994+Myzterouz@users.noreply.github.com>
2023-03-19 12:19:28 +0800

    Add files via upload

--- assets/js/myzterouz.js
+++ assets/js/myzterouz.js
@@ -7,4 +7,10 @@ $( document ).ready(function() {
 function setup(){
     $("#img1").attr("src",img1);
     $("#img2").attr("src",img2);
+    $('#img1').parent().find('.card-title').text(user1);
+    $('#img1').parent().find('.card-title').addClass("text-uppercase");
+    $('#img2').parent().find('.card-title').text(user2);
+    $('#img2').parent().find('.card-title').addClass("text-uppercase");
+    $('#img1').parent().find('.card-text').text(text1);
+    $('#img2').parent().find('.card-text').text(text2);
 }
